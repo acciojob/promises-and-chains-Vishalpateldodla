19271ba80685722c7708d3fdfd0fca18082c2eae
Vishalpateldodla <117738669+Vishalpateldodla@users.noreply.github.com>
2023-03-15 16:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+function getObject() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        name: document.getElementById('name').value,
+        age: Number(document.getElementById('age').value)
+      });
+    }, 4000);
+  });
+}
+
+function extractAge(obj) {
+  return Promise.resolve(obj.age);
+}
+
+function createMessage(age) {
+  const name = document.getElementById('name').value;
+  const message = age >= 18 ? `Welcome, ${name}. You can vote.` : `Oh sorry ${name}. You aren't old enough.`;
+  return Promise.resolve({ message });
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+  const nameInput = document.getElementById('name');
+  const ageInput = document.getElementById('age');
+  if (nameInput.checkValidity() && ageInput.checkValidity()) {
+    getObject()
+      .then(extractAge)
+      .then(createMessage)
+      .then(({ message }) => {
+        alert(message);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  } else {
+    alert('Please fill all the fields');
+  }
+});
